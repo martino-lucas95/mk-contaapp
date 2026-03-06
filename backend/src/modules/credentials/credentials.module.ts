3f007344e3dada14bf8a87b98bb089370e70c6f1
Lucas Martino <71530057+martino-lucas95@users.noreply.github.com>
2026-03-06 14:10:56 -0300

    feat: credentials module

--- backend/src/modules/credentials/credentials.module.ts
+++ backend/src/modules/credentials/credentials.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Credential } from './credential.entity';
+import { CredentialsService } from './credentials.service';
+import { CredentialsController } from './credentials.controller';
+import { EncryptionService } from '../../common/encryption.service';
 
-// TODO: Agregar TypeOrmModule.forFeature([...entidades]) y providers
 @Module({
-  imports: [],
-  providers: [],
-  controllers: [],
-  exports: [],
+  imports: [TypeOrmModule.forFeature([Credential])],
+  providers: [CredentialsService, EncryptionService],
+  controllers: [CredentialsController],
+  exports: [CredentialsService],
 })
 export class CredentialsModule {}
