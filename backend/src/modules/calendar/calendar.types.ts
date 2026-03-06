59e4580884c66486b4d5829e25c9972ce8f5d349
Lucas Martino <71530057+martino-lucas95@users.noreply.github.com>
2026-03-06 14:10:50 -0300

    feat: calendar types

---
+++ backend/src/modules/calendar/calendar.types.ts
@@ -0,0 +1,10 @@
+export interface PerfilTributario {
+  contribuyenteIva:  boolean;
+  liquidaIrae:       boolean;
+  irpfCat1:          boolean;
+  irpfCat2:          boolean;
+  empleadorBps:      boolean;
+  fonasa:            boolean;
+  cjppu:             boolean;
+  fondoSolidaridad:  boolean;
+}
