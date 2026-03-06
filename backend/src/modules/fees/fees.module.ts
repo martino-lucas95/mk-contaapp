7270a34837101c8521bc0c1f048dc41bd2f270fe
Lucas Martino <71530057+martino-lucas95@users.noreply.github.com>
2026-03-06 14:26:28 -0300

    feat: honorarios module

--- backend/src/modules/fees/fees.module.ts
+++ backend/src/modules/fees/fees.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Honorario } from './honorario.entity';
+import { FeesService } from './fees.service';
+import { FeesController } from './fees.controller';
 
-// TODO: Agregar TypeOrmModule.forFeature([...entidades]) y providers
 @Module({
-  imports: [],
-  providers: [],
-  controllers: [],
-  exports: [],
+  imports: [TypeOrmModule.forFeature([Honorario])],
+  providers: [FeesService],
+  controllers: [FeesController],
+  exports: [FeesService],
 })
 export class FeesModule {}
