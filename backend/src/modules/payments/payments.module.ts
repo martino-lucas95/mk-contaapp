a19cb9c1629567f2cbede2271512dfa28aa59af3
Lucas Martino <71530057+martino-lucas95@users.noreply.github.com>
2026-03-06 14:40:37 -0300

    feat: payments module

--- backend/src/modules/payments/payments.module.ts
+++ backend/src/modules/payments/payments.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module }            from '@nestjs/common';
+import { TypeOrmModule }     from '@nestjs/typeorm';
+import { BoletoPago }        from './boleto-pago.entity';
+import { Client }            from '../clients/client.entity';
+import { PaymentsService }   from './payments.service';
+import { PaymentsController }from './payments.controller';
 
-// TODO: Agregar TypeOrmModule.forFeature([...entidades]) y providers
 @Module({
-  imports: [],
-  providers: [],
-  controllers: [],
-  exports: [],
+  imports: [TypeOrmModule.forFeature([BoletoPago, Client])],
+  providers: [PaymentsService],
+  controllers: [PaymentsController],
+  exports: [PaymentsService],
 })
 export class PaymentsModule {}
