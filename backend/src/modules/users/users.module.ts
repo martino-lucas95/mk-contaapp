c3964c39004cbce7b10e49c777f501ac54271ee6
Lucas Martino <71530057+martino-lucas95@users.noreply.github.com>
2026-03-06 14:40:39 -0300

    feat: users module

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
-// TODO: Agregar TypeOrmModule.forFeature([...entidades]) y providers
 @Module({
-  imports: [],
-  providers: [],
-  controllers: [],
-  exports: [],
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UsersService],
+  controllers: [UsersController],
+  exports: [UsersService],
 })
 export class UsersModule {}
